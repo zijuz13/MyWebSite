d582a16e86375c5bf48bd02bb93ba4c3a16c49fa
zijuz13 <zijuz13@uci.edu>
2020-12-10 21:55:33 +0800

    Added Blog page with es

--- js/article.js
+++ js/article.js
@@ -1,8 +1,13 @@
 const Article={
     url:'http://localhost:10010/api/blog/blog',
+    searchUrl:'http://localhost:10010/api/search/blogs/all',
     fetchArticle(data){
-       return axios.get(this.url,{
+        data.status='published'
+       return axios.get(this.searchUrl,{
            params:data
        })
+    },
+    fetchArticleById(id){
+        return axios.get(this.url+"/"+id);
     }
 }
