e56290fc82e66b332ba759ea1bc85d22f20e8529
zijuz13 <zijuz13@uci.edu>
2020-12-11 22:47:32 +0800

    Finished Search Pages

--- js/article.js
+++ js/article.js
@@ -2,10 +2,10 @@ const Article={
     url:'http://localhost:10010/api/blog/blog',
     searchUrl:'http://localhost:10010/api/search/blogs/all',
     fetchArticle(data){
-        data.status='published'
-       return axios.get(this.searchUrl,{
-           params:data
-       })
+        // data.status='published'
+       return axios.post(this.searchUrl,
+           huajun.stringify(data)
+       )
     },
     fetchArticleById(id){
         return axios.get(this.url+"/"+id);
