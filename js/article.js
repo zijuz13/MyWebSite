6840e15d5eac3198e638507ebcdb92bd10b5973d
Zijun Zhou <70651186+zijuz13@users.noreply.github.com>
2021-01-20 15:28:00 +0800

    Update article.js

--- js/article.js
+++ js/article.js
@@ -1,6 +1,6 @@
 const Article={
-    url:'http://101.132.194.179:10010/api/blog/blog',
-    searchUrl:'http://101.132.194.179:10010/api/search/blogs/all',
+    url:'https://www.huajun123.com/api/blog/blog',
+    searchUrl:'https://www.huajun123.com/api/search/blogs/all',
     fetchArticle(data){
         // data.status='published'
        return axios.post(this.searchUrl,
