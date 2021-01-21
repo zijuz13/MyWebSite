3716a30c43092941c4250ef9622ff6b842f4bb85
Zijun Zhou <70651186+zijuz13@users.noreply.github.com>
2021-01-21 10:51:02 +0800

    Update article.js

--- js/article.js
+++ js/article.js
@@ -1,5 +1,5 @@
 const Article={
-    url:'https://www.huajun123.com/api/blog/blog',
+    url:'https://www.huajun123.com/api/item/blog',
     searchUrl:'https://www.huajun123.com/api/search/blogs/all',
     fetchArticle(data){
         // data.status='published'
